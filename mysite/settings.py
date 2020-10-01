4c333df52c45543d383088f0113bf4207631b383
unknown <tomasz.solarewicz@gmail.com>
2020-10-01 20:01:12 +0200

    test25

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'homepage:forum'
 LOGIN_URL = 'login'
