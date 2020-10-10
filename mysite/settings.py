2df151fd417cefeebbe3c5de1be02b434c5c9615
unknown <tomasz.solarewicz@gmail.com>
2020-10-10 12:09:27 +0200

    aws settings first try

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'sorl.thumbnail',
     'tinymce',
+    'storages',
 
 ]
 
@@ -126,10 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'homepage:forum'
@@ -138,3 +139,28 @@ LOGOUT_URL = 'logout'
 THUMBNAIL_DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
+
+AWS_ACCESS_KEY_ID = 'AKIAIMYB66FT4YPSJYWA'
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = 'django-from-new-repo'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION1 = 'static'
+AWS_LOCATION2 = 'media'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'homepage/static'),
+]
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION1)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/media'),
+    ]
+
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION2)
+MEDIAFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
