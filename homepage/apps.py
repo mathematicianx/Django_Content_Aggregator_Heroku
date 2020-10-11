b3f2322b08cf35b70fad69e649c7bfc3a0a75615
unknown <tomasz.solarewicz@gmail.com>
2020-10-11 19:33:46 +0200

    heroku updater

--- homepage/apps.py
+++ homepage/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class HomepageConfig(AppConfig):
     name = 'homepage'
-    from . import custom_webscraper
-
-    def ready(self):
-        from homepage import automat
-        automat.start()
--- homepage/automat.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from homepage import run_webscraper
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(run_webscraper.save_data_from_scrapers(), 'interval', minutes=5)
-    scheduler.start()
