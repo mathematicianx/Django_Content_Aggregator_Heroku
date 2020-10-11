99aa38357a7034570069e5923efc321623003514
unknown <tomasz.solarewicz@gmail.com>
2020-10-11 10:53:19 +0200

    aws safety

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,21 +140,10 @@ THUMBNAIL_DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-
-AWS_ACCESS_KEY_ID = 'AKIAIMYB66FT4YPSJYWA'
-AWS_SECRET_ACCESS_KEY = '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'
-
 AWS_STORAGE_BUCKET_NAME = 'django-from-new-repo'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
  }
-# AWS_LOCATION = 'static'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'homepage/static'),
-# ]
-#
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
