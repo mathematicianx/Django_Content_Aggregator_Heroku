9e321a11ad1369773d822a10845acb3e1524e89b
Tomasz Solarewicz <tomasz.solarewicz@gmail.com>
2021-01-03 17:56:34 +0100

    created serializer for news

--- homepage/api/serializers.py
+++ homepage/api/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from ..models import News
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ('title', 'link', 'which_site', 'date_of_publication', 'content')
+
