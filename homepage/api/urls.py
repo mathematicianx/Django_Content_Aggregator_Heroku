59119d1c4171ac2cf296d473cbfd52381f528e49
Tomasz Solarewicz <tomasz.solarewicz@gmail.com>
2021-01-04 19:33:43 +0100

    Create news from existing JSON data functionality added

--- homepage/api/urls.py
+++ homepage/api/urls.py
@@ -5,5 +5,6 @@ app_name = 'api'
 
 urlpatterns = [
     path('news/', views.NewsListView.as_view(), name='news_list'),
-    path('news/<pk>/', views.NewsDetailView.as_view(), name='news_detail'),
+    path('news/<pk>/', views.DetailedView.as_view(), name='news_detail'),
+    path('news-create/', views.CreateNewsView.as_view(), name='news_create'),
 ]
--- homepage/api/views.py
+++ homepage/api/views.py
@@ -1,12 +1,29 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from ..models import News
 from .serializers import NewsSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class NewsListView(generics.ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+#
+# class NewsDetailView(generics.RetrieveAPIView):
+#     queryset = News.objects.get()
+#     serializer_class = NewsSerializer
 
-class NewsDetailView(generics.RetrieveAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
+class DetailedView(APIView):
 
+    def get(self, request, pk):
+        queryset = get_object_or_404(News, pk=pk)
+        serializer = NewsSerializer(queryset)
+        return Response(serializer.data)
+
+class CreateNewsView(APIView):
+
+    def post(self, request):
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
